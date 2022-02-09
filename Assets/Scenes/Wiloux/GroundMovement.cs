4d85b2612a03e4bbae57ce47c4978ace8cd979fe
Benjamin Chariou <chariou.benjamin@gmail.com>
2022-02-09 09:44:03 +0100

    Intégration Dash

--- Assets/Scenes/Wiloux/GroundMovement.cs
+++ Assets/Scenes/Wiloux/GroundMovement.cs
@@ -84,21 +84,27 @@ public class GroundMovement : MonoBehaviour
     [Space(10)]
 
     [Header("Dash")]
-    //Dash
-    public Transform dashTarget;
-    public float dashForce;
+    public bool dashing;
+    public float maxDashDistance = 5.0f;
+    private float dashDistance = 5.0f;
+    public float dashDuration = 0.15f;
+    public Target[] dashTargets;
+    public Target currentTarget;
+    Target lastTarget;
+    [Space(10)]
+
+    [Header("Glide")]
+    public Camera myCam;
+    public float glideSpeed = 12.5f;
+    public float drag = 6f;
+    public Vector3 myRotation;
+    public float percentage;
+    public float quickSpeed;
+    public float inputTest;
     [Space(10)]
 
     public Transform groundSphere;
     public float sphereRadius;
-
-
-    public float speed = 12.5f;
-    public float drag = 6f;
-
-    public Vector3 myRotation;
-
-    public float percentage;
 
     public bool wantsGlinding;
 
@@ -117,6 +123,13 @@ public class GroundMovement : MonoBehaviour
 
     void Start()
     {
+        dashTargets = FindObjectsOfType<Target>();
+        currentTarget = GetClosestEnemy(dashTargets);
+        if (currentTarget != null)
+        {
+            currentTarget.MakeActive(true);
+            lastTarget = currentTarget;
+        }
         playerScale = transform.localScale;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -153,7 +166,7 @@ public class GroundMovement : MonoBehaviour
         }
         else if (wantsGlinding && !grounded)
         {
-            // Glid();
+            Glid();
         }
 
         //QTE ACCELERATION
@@ -222,7 +235,24 @@ public class GroundMovement : MonoBehaviour
             SideJump();
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftShift))
+        currentTarget = GetClosestEnemy(dashTargets);
+        if (currentTarget != lastTarget)
+        {
+            if (lastTarget != null)
+            {
+                lastTarget.MakeActive(false);
+            }
+            if (currentTarget != null)
+            {
+                currentTarget.MakeActive(true);
+                lastTarget = currentTarget;
+            }
+            else
+            {
+                lastTarget = null;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && currentTarget != null)
         {
             Dash();
         }
@@ -257,6 +287,9 @@ public class GroundMovement : MonoBehaviour
             StartCrouch();
         if (Input.GetKeyUp(KeyCode.LeftControl))
             StopCrouch();
+
+       
+
     }
 
     #region Movement
@@ -362,33 +395,31 @@ public class GroundMovement : MonoBehaviour
     //        transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.y, 0));
     //    }
     //}
-    //private void Glid()
-    //{
-    //    myRotation.x += 20 * Input.GetAxis("Vertical") * Time.deltaTime;
-    //    myRotation.x = Mathf.Clamp(myRotation.x, -20, 45);
+    private void Glid()
+    {
+        myRotation.x += 20 * Input.GetAxis("Vertical") * Time.deltaTime;
+        myRotation.x = Mathf.Clamp(myRotation.x, -20, 45);
 
-    //    myRotation.y += 20 * Input.GetAxis("Horizontal") * Time.deltaTime;
+        myRotation.y += 40 * Input.GetAxis("Horizontal") * Time.deltaTime;
 
-    //    myRotation.z = -5 * Input.GetAxis("Horizontal");
-    //    myRotation.z = Mathf.Clamp(myRotation.z, -5, 5);
+        myRotation.z = -5 * Input.GetAxis("Horizontal");
+        myRotation.z = Mathf.Clamp(myRotation.z, -30, 30);
 
-    //    orientation.transform.rotation = Quaternion.Euler(myRotation);
-    //    transform.rotation = Quaternion.Euler(myRotation);
+        transform.rotation = Quaternion.Euler(myRotation);
 
-    //    percentage = myRotation.x / 45;
+        percentage = myRotation.x / 45;
 
-    //    float modifiedDrag = (percentage * -2) + 6;
-    //    float modifiedSpeed = ((speed + speedModifier) - speed) + speed;
+        float modifiedDrag = (percentage * -2) + 6;
+        float modifiedSpeed = ((glideSpeed + 3.6f) - glideSpeed) + glideSpeed;
 
-    //    rb.drag = modifiedDrag;
-    //    Vector3 localV = transform.InverseTransformDirection(rb.velocity);
-    //    localV.z = modifiedSpeed;
-    //    rb.velocity = transform.TransformDirection(localV);
+        rb.drag = modifiedDrag;
+        Vector3 localV = transform.InverseTransformDirection(rb.velocity);
+        localV.z = modifiedSpeed + 20 * Input.GetAxis("Vertical") * Time.deltaTime;
+        //Debug.Log(localV.z);
+        rb.velocity = transform.TransformDirection(localV);
 
-    //    playerCam.transform.localRotation = Quaternion.Euler(myRotation);
-    //    orientation.transform.localRotation = Quaternion.Euler(localV);
 
-    //}
+    }
 
     private void CounterMovement(float x, float y, Vector2 mag)
     {
@@ -485,9 +516,9 @@ public class GroundMovement : MonoBehaviour
     {
 
 
-        speed = speed * 2;
+        glideSpeed = glideSpeed * 2;
         yield return new WaitForSeconds(waitTime);
-        speed = speed / 2;
+        glideSpeed = glideSpeed / 2;
         yield return null;
 
 
@@ -689,7 +720,77 @@ public class GroundMovement : MonoBehaviour
     #region Dash
     void Dash()
     {
-        rb.AddForce((dashTarget.position - transform.position).normalized * dashForce, ForceMode.VelocityChange);
+
+        var direction = (currentTarget.transform.position - this.transform.position);
+
+        // Making sure we have a reasonable vector here
+        if (direction.magnitude >= 0.1f)
+        {
+            // Don't exceed the target, you might not want this
+            this.StartCoroutine(this.DashRoutine(direction.normalized));
+        }
+    }
+
+    IEnumerator DashRoutine(Vector3 direction)
+    {
+        // Account for some edge cases   
+        if (this.dashDistance <= 0.001f)
+            yield break;
+
+        if (this.dashDuration <= 0.001f)
+        {
+            this.transform.position += direction * this.dashDistance;
+            yield break;
+        }
+
+        // Update our state
+        this.dashing = true;
+        var elapsed = 0f;
+        var start = this.transform.position;
+        var target = this.transform.position + this.dashDistance * direction;
+
+        // There are a few different ways to do this, but I've always preferred
+        // Lerp for things that have a fixed duration as the interpolant is clear
+        while (elapsed < this.dashDuration)
+        {
+            var iterTarget = Vector3.Lerp(start, target, elapsed / this.dashDuration);
+            this.transform.position = iterTarget;
+
+            yield return null;
+            elapsed += Time.deltaTime;
+        }
+
+        // Snap there when we finish then update our state
+        this.transform.position = target;
+        this.dashing = false;
+    }
+
+    Target GetClosestEnemy(Target[] target)
+    {
+        Target bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        foreach (Target potentialTarget in target)
+        {
+            Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+        dashDistance = Vector3.Distance(bestTarget.transform.position, Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 0)));
+
+        if (dashDistance <= maxDashDistance && bestTarget.isVisible == true)
+        {
+            return bestTarget;
+        }
+        else
+        {
+            return null;
+        }
+
     }
     #endregion
 }
