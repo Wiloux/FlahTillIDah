1c2396a6f1ff4f163dafdecd4bdd4c9679a554cf
Wiloux <54811449+Wiloux@users.noreply.github.com>
2022-02-09 09:22:31 +0100

    3rd person movements i guess

--- Assets/Script/CamShake.cs
+++ Assets/Script/CamShake.cs
@@ -12,18 +12,18 @@ public class CamShake : MonoBehaviour
     private void Start()
     {
         originalPos = transform.localPosition;
-        
+
     }
 
     private void LateUpdate()
     {
-        if (moveController.wantsGlinding) { 
-        float modifiedShaking =  shaking * moveController.percentage;
-        transform.localPosition = originalPos + new Vector3(Random.Range(-modifiedShaking, modifiedShaking), Random.Range(-modifiedShaking, modifiedShaking), 0);
-        myCam = gameObject.GetComponent<Camera>();
+        if (moveController.wantsGlinding)
+        {
+            float modifiedShaking = shaking * moveController.percentage;
+            transform.localPosition = originalPos + new Vector3(Random.Range(-modifiedShaking, modifiedShaking), Random.Range(-modifiedShaking, modifiedShaking), 0);
+            myCam = gameObject.GetComponent<Camera>();
             myCam.fieldOfView = moveController.myRotation.x + 80;
-            Debug.Log(myCam.fieldOfView);
-        myCam.fieldOfView = Mathf.Clamp(myCam.fieldOfView,60, 80);
+            myCam.fieldOfView = Mathf.Clamp(myCam.fieldOfView, 60, 80);
         }
     }
 }
